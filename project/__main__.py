dbc275bb5c09605318765ab35d4d5c41c6b7a48d
ub808 <1808ub@gmail.com>
2021-11-26 10:06:31 -0700

    added view capability

--- project/__main__.py
+++ project/__main__.py
@@ -1,10 +1,12 @@
 import arcade
-from game.director import TeamGame
+from game import constants
+from game.views import InstructionView
 
 def main():
         """ Main Function """
-        newGame = TeamGame()
-        newGame.setup()
+        window = arcade.Window(constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT, constants.SCREEN_TITLE)
+        start_view = InstructionView()
+        window.show_view(start_view)
         arcade.run()
 
 if __name__ == "__main__":
---
+++ project/game/views.py
@@ -0,0 +1,27 @@
+import arcade
+from game import constants
+from game.director import GameView
+
+class InstructionView(arcade.View):
+
+    def on_show(self):
+        #this is run once we swithc to this view
+        arcade.set_background_color(arcade.csscolor.DARK_KHAKI)
+
+        #reset the viewport
+        arcade.set_viewport(0, constants.SCREEN_WIDTH, 0, constants.SCREEN_HEIGHT)
+
+    def on_draw(self):
+        """ Draw this view"""
+        arcade.start_render()
+        arcade.draw_text("Instructions Screen", constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2,
+                         arcade.color.WHITE, font_size=50, anchor_x="center")
+        arcade.draw_text("Click to advance",  constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2-75,
+                         arcade.color.WHITE, font_size=20, anchor_x="center")
+
+    def on_mouse_press(self, _x, _y, _button, _modifiers):
+        """ if the user presses the mouse, start the game"""
+        game_view = GameView()
+        game_view.setup()
+        self.window.show_view(game_view)
+        
